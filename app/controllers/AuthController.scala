6b3cc29891bab0a25e9f77eba1ac0b075a6e78ce
Pommel Kurama <pommel.pg@gmail.com>
2017-02-20 02:21:29 +0900

    auth match

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import dao.UserDao
+import models.Tables.UserRow
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{Action, Controller}
 
@@ -10,7 +11,13 @@ import play.api.mvc.{Action, Controller}
 	* Created by admin on 2017/02/19.
 	*/
 class AuthController @Inject()(userDao: UserDao) extends Controller{
-	def auth(id: String) = Action.async{ implicit rs =>
-		for(Some(user) <- userDao.findById(id)) yield Ok(views.html.user(user))
+
+	def auth(id: String) = Action.async{ implicit request =>
+		for(Some(user) <- userDao.findById(id)) yield
+			user match {
+				case _: UserRow => Ok(views.html.user(user))
+				case _ => Ok(views.html.user(new UserRow("", "", None, None)))
+		}
 	}
+
 }
